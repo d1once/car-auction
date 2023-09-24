9b4a90093dbbae58dd9de09a6875c97828166188
Dionis <d.senja.ext@teamsystem.com>
2023-09-24 19:54:38 +0200

    Initial Commit

---
+++ src/AuctionService/Data/Migrations/20230923200829_InitialCreateV2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Items",
+                newName: "Year");
+
+            migrationBuilder.RenameColumn(
+                name: "Milage",
+                table: "Items",
+                newName: "Mileage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Items",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "Year",
+                table: "Items",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "Mileage",
+                table: "Items",
+                newName: "Milage");
+        }
+    }
+}
